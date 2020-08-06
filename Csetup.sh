fa5c36f9a5c2e29c7bfcd23dcdcb3ad55d1aab11
ChesterFritz <53992457+ChesterFritz@users.noreply.github.com>
2020-08-06 11:01:29 +0800

    Update Csetup.sh

--- Csetup.sh
+++ Csetup.sh
@@ -49,10 +49,7 @@ sudo apt update && sudo apt upgrade -y
 sudo apt-get update
 sudo apt-get install git
 git --version
-git version 2.9.2
 
-git config --global user.name "Emma Paris"
-git config --global user.email "eparis@atlassian.com"
 
 # Message:
 zenity --info --text="All Done!"
